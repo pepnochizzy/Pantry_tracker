18b941b5344bc807994fc1d96ac51fa969f7f98f
pepnochizzy <laragray@live.co.uk>
2026-04-15 18:50:31 +0100

    feat: GET endpoint added

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,1 +1,28 @@
 import { db } from "@/utils/dbConnections";
+
+//GET route
+
+export async function GET() {
+  try {
+    const res = await db.query(`SELECT * FROM recipes ORDER BY name`);
+    //handle empty db
+    const result = res.rows || [];
+
+    return new Response(JSON.stringify({ success: true, data: result }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    console.error("Failed to fetch recipes:", err);
+    return new Response(
+      JSON.stringify({ success: false, error: "Failed to fetch task" }),
+      { status: 500, headers: { "Content-type": "application/json" } },
+    );
+  }
+}
+
+//create new recipe
+export async function POST(req) {
+  try {
+  } catch (err) {}
+}
