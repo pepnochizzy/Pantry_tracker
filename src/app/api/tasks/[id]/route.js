cd9de04dd0ff6a45492ca3c8db329daca7d716d0
pepnochizzy <laragray@live.co.uk>
2026-04-24 21:12:01 +0100

    feat: added recipe DELETE route, tested with Postman. Next steps: adding user deletion, will delete pantry but keep recipes

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,1 +1,41 @@
-import { db } from "@/utils/dbConnections";
+import { db } from "@/utils/dbConnection";
+
+export async function DELETE(req, { params }) {
+  //only needs to delete from recipe and recipe_ingredient table, ingredients can stay.
+  //implementing cascade in database
+  const { id } = await params;
+  console.log("Deleting task with id:", id);
+
+  try {
+    const data = await db.query(
+      `
+        DELETE FROM recipes WHERE id = $1
+        RETURNING *`,
+      [id],
+    );
+    if (data.rowCount === 0) {
+      return new Response(
+        JSON.stringify(
+          { success: false, error: "Recipe not found, cannot delete" },
+          { status: 404 },
+        ),
+      );
+    }
+    return new Response(
+      JSON.stringify({ success: true, data: `Rows deleted: ${data.rowCount}` }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (err) {
+    console.error("Failed to delete recipes and related content:", err);
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Failed to to delete recipes and related content",
+      }),
+      { status: 500, headers: { "Content-type": "application/json" } },
+    );
+  }
+}
---
+++ src/app/api/tasks/[user_id]/route.js
@@ -0,0 +1,3 @@
+import { db } from "@/utils/dbConnection";
+
+//delete user account - cascades to users pantry
