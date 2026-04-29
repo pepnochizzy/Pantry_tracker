6b5f2e1f7e2461ded2907ef2407642e65b05b294
pepnochizzy <laragray@live.co.uk>
2026-04-29 16:29:45 +0100

    feat: Added delete route for recipe, table has cascade so it will delete linked data from recipe_ingredient table too

--- src/app/api/tasks/[user_id]/route.js
+++ src/app/api/tasks/[user_id]/route.js
@@ -1,3 +1,4 @@
 import { db } from "@/utils/dbConnection";
 
 //delete user account - cascades to users pantry
+//Don't want recipes to be deleted, future feature will be other users being able to save recipes from others. If a user deletes their account, we want the recipe to remain.
